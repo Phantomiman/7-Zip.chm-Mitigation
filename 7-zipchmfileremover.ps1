8b6d8e287b151e7636b9962fea0020052ba86a7a
Phantomiman <119011920+Phantomiman@users.noreply.github.com>
2022-12-20 17:34:13 +0200

    Add files via upload

---
+++ 7-zipchmfileremover.ps1
@@ -0,0 +1,8 @@
+﻿# This script made by Sami
+if (Test-Path "C:\Program Files\7-Zip") {
+  $chmFile = "C:\Program Files\7-Zip\7-zip.chm"
+
+  if (Test-Path $chmFile) {
+    Remove-Item $chmFile
+  }
+}
